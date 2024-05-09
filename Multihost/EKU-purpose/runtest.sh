d54071e6941ec1ca7dc593a5064396e3e2441ecc
koncpa <pkoncity@redhat.com>
2024-05-09 15:28:07 +0200

    Fix variable passing
    Variable SERVER/CLIENT_IP get before
    empty string. Pass directly from
    SERVER/CLIENT var IP of host.

--- Multihost/EKU-purpose/runtest.sh
+++ Multihost/EKU-purpose/runtest.sh
@@ -114,8 +114,8 @@ rlJournalStart
           CleanupRegister "rlRun 'kill $(pidof rngd)'"
         }
         # prepare certificates
-        SERVER_IP=`host $SERVERS | awk '/has address/ {print $NF;exit}'`
-        CLIENT_IP=`host $CLIENTS | awk '/has address/ {print $NF;exit}'`
+        SERVER_IP=$SERVERS
+        CLIENT_IP=$CLIENTS
 
         cat > ca.tmpl <<EOF
 organization = "Red Hat"
